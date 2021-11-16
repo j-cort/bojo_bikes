860d4586ea028d1b613669f571c2e636a1e79469
j-cort <81960011+j-cort@users.noreply.github.com>
2021-11-16 23:04:29 +0000

    RGR to ensure docking stations can release bike objects and bike objects return true when asked if working.

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,4 +4,12 @@ describe DockingStation do
   it 'allows user to release a bike' do
     expect(subject).to respond_to :release_bike
   end
+  it 'returns an instance of the "Bike" class when release_bike is called' do
+    bike = subject.release_bike
+    expect(bike).to be_instance_of(Bike)
+  end
+  it 'returns true when asked if its released bike is working' do
+    bike = subject.release_bike
+    expect(bike.working?).to eq true
+  end
 end
