34dd7e278f36639b1ec1c82e811d11d409d68592
j-cort <81960011+j-cort@users.noreply.github.com>
2021-11-17 01:34:57 +0000

    RGR - add  can dock bike, can check if bike available functionality

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,6 +1,20 @@
-require 'bike'
+require_relative 'bike'
+
 class DockingStation
+  attr_reader :rack
+  def initialize
+    @rack = []
+  end
+
   def release_bike
     Bike.new
+  end
+
+  def dock_bike(bike)
+    @rack << bike
+  end
+
+  def bike_available?
+    @rack.empty? ? false : true
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -12,4 +12,31 @@ describe DockingStation do
     bike = subject.release_bike
     expect(bike.working?).to eq true
   end
+  it 'allows user to dock a bike (instance)' do
+    expect(subject).to respond_to(:dock_bike).with(1).argument
+  end
+  it 'has a rack to store docked bikes' do
+    expect(subject.instance_variable_defined?(:@rack)).to eq true
+  end
+  it 'rack uses an array to store docked bikes' do
+    expect(subject.rack.class).to eq Array
+  end
+  # later refactor above two tests to use the have_attributes matcher
+  # https://relishapp.com/rspec/rspec-expectations/v/3-10/docs/built-in-matchers/have-attributes-matcher
+  it 'stores "bike" in the rack when dock_bike(bike) method is called' do
+    bike = subject.release_bike
+    subject.dock_bike(bike)
+    expect(subject.rack.include?(bike)).to eq true
+  end
+  it 'allows users to check if bikes are available' do
+    expect(subject).to respond_to :bike_available? 
+  end
+  it 'returns true if a bike is available' do
+    bike = subject.release_bike
+    subject.dock_bike(bike)
+    expect(subject.bike_available?).to eq true
+  end
+  it 'returns false if a bike is not available' do
+    expect(subject.bike_available?).to eq false
+  end
 end
